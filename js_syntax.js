93d929c4ddb408291f9a670e757e90e47f7d359a
gnaoblin <benbowsparkt@gmail.com>
2024-01-19 13:06:34 +0000

    messing with javascript ann mor html

---
+++ js_syntax.js
@@ -0,0 +1,12 @@
+//normal comment notation yayyy
+/*
+variable names are case sensitive
+*/
+let test = "test"
+let zest = "grrr", homo = "rahhhhh";
+const meoww = 2, thing = 3;
+console.log("Hello World (zesty)");
+
+//variables do not have to be strongly typed
+let nothin = null;
+
--- the_foreign_works.js
+++ the_foreign_works.js
@@ -2,3 +2,4 @@ function the_foreign_works(){
 
     document.getElementById("test").innerHTML = "the foreign works"
 }
+
