a32571b4fed92ea627ed41e36b7446ff12a1efba
gnaoblin <benbowsparkt@gmail.com>
2024-01-25 12:14:06 +0000

    please god why

--- add_tab_iframe.js
+++ add_tab_iframe.js
@@ -1,21 +1,26 @@
-var tab_container = []
-const tab_holder = document.getElementById("tab_holder")
+var tab_container = [];
+const tab_holder = document.getElementById("tab_holder");
 
 function add_tab()
 {
-    let new_tab = document.createElement("button");
-    //new_tab.classlist.add("tab"); other way to add class
-    new_tab.onclick = open_tab;
-    new_tab.setAttribute("class", "tab");
+    
+    let new_tab = document.createElement("div");
+    new_tab.setAttribute("class","tab");
+
+    let tab_open_page = document.createElement("div");
+    tab_open_page.onclick = open_tab;
+    tab_open_page.setAttribute("class", "open_page_button");
+    tab_open_page.textContent = "whadad"
 
     let tab_close_button = document.createElement("span");
     tab_close_button.onclick = close_tab;
     tab_close_button.setAttribute("class", "close_button");
-    tab_close_button.innerHTML("&times");
+    tab_close_button.innerHTML= "&times"
 
     tab_container.push(new_tab);
     tab_holder.appendChild(new_tab);
-    new_tab.appendChild(tab_close_button);
+    new_tab.appendChild(tab_open_page);
+    tab_open_page.appendChild(tab_close_button);
 }
 
 function open_tab()
@@ -28,6 +33,6 @@ function open_tab()
 function close_tab()
 {
 
-
+    console.log("ZAMN")
 
 }
---
+++ resizing.js
@@ -0,0 +1,49 @@
+window.onload = initial_sizing;
+
+function initial_sizing()
+{
+    
+    let screen_height = window.screen.availHeight
+    let screen_width = window.screen.availWidth
+
+    let tab_holder_height = screen_height * 0.1
+
+    let game_height = screen_height * 0.7;
+
+    let game_space_height = screen_height * 0.8;
+    let game_space_width = screen_width * 0.7;
+
+    //let tab_height = document.documentElement.clientHeight * 0.1;
+    //let tab_width = document.documentElement.clientWidth * 0.7;
+
+    //let sidebar_height = document.documentElement.clientHeight * 1;
+    let sidebar_width = screen_width * 0.2;
+
+    let game_space = document.getElementById("game_space");
+
+    game_space.style.minHeight = game_space_height + "px";
+    game_space.style.minWidth = game_space_width + "px";
+
+    let tab_holder = document.getElementById("tab_holder");
+
+    tab_holder.style.minHeight = tab_holder_height + "px";
+    tab_holder.style.minWidth = game_space_width + "px";
+
+    let game = document.getElementById("game");
+
+    game.style.minHeight = game_height + "px";
+    game.style.minWidth = game_width + "px";
+
+    console.log(game_height);
+    console.log("worky");
+
+    /*
+    let tab = document.getElementById("tab_holder");
+    tab.style.minHeight = tab_height + "px";
+    tab.style.minWidth = tab_width + "px";
+    */
+
+    let sidebar = document.getElementById("sidebar");
+    
+    sidebar.style.minWidth = sidebar_width + "px";
+}
